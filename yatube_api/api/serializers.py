93f93b9cbb5f0251da96b09a77b2e45e252ababc
Мухаммад Тагаев <Tagaevmukhammad@gmail.com>
2025-03-25 23:05:30 +0300

    on_review_1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -11,7 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group',)
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -22,11 +22,11 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created',)
+        fields = ('id', 'author', 'post', 'text', 'created')
         read_only_fields = ('post',)
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('id', 'title', 'slug', 'description',)
+        fields = ('id', 'title', 'slug', 'description')
